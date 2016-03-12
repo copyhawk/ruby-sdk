5a1d9a68d0bb51329bb582da1cae28a8a3382d73
Copyhawk Team <jonatan@copyhawk.co>
2016-03-11 20:53:27 -0500

    Valid URL

--- lib/copy_hawk/hawk_api.rb
+++ lib/copy_hawk/hawk_api.rb
@@ -2,7 +2,7 @@ require 'httparty'
 
 class HawkApi
   include HTTParty
-  @@base_uri = "http://localhost:3000/api"
+  @@base_uri = "http://app.copyhawk.co/api"
 
   def single_staging_script(token, label, lang)
     response = self.class.get("#{@@base_uri}/staging/#{token}/#{label}?lang=#{lang}",
